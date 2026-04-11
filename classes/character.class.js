79885f2ed3aca70d272fa479b533a38cad7c32e2
Florian Narr <f.narr@gmx.de>
2026-04-11 15:28:37 +0200

    Implement character and enemy classes, add game world structure, and update HTML for new scripts

---
+++ classes/character.class.js
@@ -0,0 +1,13 @@
+class Character extends MovableObject {
+
+    constructor() {
+        super();
+        this.loadImage('img/2_character_pepe/1_walk/W-21.png');
+    }   
+  
+    jump() {
+
+    }
+
+
+}
