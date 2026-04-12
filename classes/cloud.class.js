6c9902e00bf75d1501819151d2f0743e6d73c6cc
Florian Narr <f.narr@gmx.de>
2026-04-12 11:54:06 +0200

    Refactor Character and Cloud classes, implement animation logic, and enhance MovableObject structure

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,13 +1,21 @@
 class Cloud extends MovableObject {
-    y = 50;     
+    y = 40;     
     height = 250;
     width = 500;    
 
     constructor() {
         super();
         this.loadImage('/img/5_background/layers/4_clouds/1.png');  
+        this.x = Math.random() * 500; // Start position between 500 and 1000 
+        this.animate(); // Start animation immediately 
+           
 
-        this.x = Math.random() * 500;
     }
     
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+            
+        }, 1000 / 60); // Move left at 60 frames per second
+    }
 }
